1bd1477b295eeed057366fbb83964453b4c36ec4
Graham Farrell <grahamfarrell1984@gmail.com>
2020-04-08 23:42:58 +0100

    Add gem code.

--- lib/greeting_generator.rb
+++ lib/greeting_generator.rb
@@ -1,6 +1,15 @@
 require "greeting_generator/version"
 
 module GreetingGenerator
-  class Error < StandardError; end
-  # Your code goes here...
+    class Generator
+        def self.generateGreeting
+            if Time.now.hour >= 0 and Time.now.hour < 12
+                '<h2>Good morning!</h2>'.html_safe
+            elsif Time.now.hour >= 12 and Time.now.hour < 18
+                '<h2>Good afternoon!</h2>'.html_safe
+            else
+                '<h2>Good evening!</h2>'.html_safe
+            end
+        end
+    end
 end
